9a40d672419b480041227126016f500d81afcc62
Collin Dever <collin@iovox.com>
2018-06-13 10:03:38 -0700

    added laptop moved into position shrunk cup but still to big

--- static/main.js
+++ static/main.js
@@ -1,16 +1,18 @@
 var cupCanvas = document.getElementById('coffee-cup');
+var laptopCanvas = document.getElementById('laptop');
 var creamAngle = Math.PI / 180;
-var creamRadius = 55;
-var creamX = 180;
-var creamY = 120;
+var creamRadius = 35;
+var creamX = 100;
+var creamY = 60;
 var creamAcceleration = 1;
 var accelerateCream = false;
 var t = 0;
-var cx = 125;
-var cy = 120;
+var cx = 70;
+var cy = 60;
 
 
 function init(){
+  drawLaptop();
   window.requestAnimationFrame(drawCoffeeCup);
 }
 
@@ -25,7 +27,7 @@ function drawCoffeeCup(){
 
     // make the bottom of the cup
     cup.beginPath();
-    cup.arc(100,225,70,.8,3.5);
+    cup.arc(60,125,35,.8,3.5);
     cup.closePath();
     cup.fillStyle = '#c7cb74';
     cup.fill();
@@ -33,30 +35,30 @@ function drawCoffeeCup(){
     //make the cup sides
     cup.fillStyle = '#c7cb74';
     cup.beginPath();
-    cup.moveTo(25,155);
-    cup.lineTo(205,190);
-    cup.lineTo(145,280);
-    cup.lineTo(30,220);
+    cup.moveTo(20,80);
+    cup.lineTo(110,90);
+    cup.lineTo(84,150);
+    cup.lineTo(25,120);
     cup.fill();
 
     // put the coffee in the cup
     cup.fillStyle = '#522f2e';
     cup.beginPath();
-    cup.arc(125,120,100,0,2*Math.PI);
+    cup.arc(70,60,50,0,2*Math.PI);
     cup.fill();
 
     // make the cup rim
-    var radGrd = cup.createRadialGradient(125,120,100,125,120,90);
+    var radGrd = cup.createRadialGradient(70,60,50,70,60,45);
     radGrd.addColorStop(0,"#f5f5ad");
     radGrd.addColorStop(1,"white");
     cup.strokeStyle = radGrd;
-    cup.lineWidth = 20;
+    cup.lineWidth = 10;
     cup.stroke();
 
     //add the cream
     cup.fillStyle = '#f9face';
     cup.beginPath();
-    cup.arc(creamX,creamY,10,0,2*Math.PI);
+    cup.arc(creamX,creamY,5,0,2*Math.PI);
     cup.fill();
 
     window.requestAnimationFrame(drawCoffeeCup);
@@ -80,6 +82,71 @@ function animateCoffeeCup(){
   // calculate the new ball.x / ball.y
   creamX = cx + creamRadius * Math.cos(creamAngle);
   creamY = cy + creamRadius * Math.sin(creamAngle);
+}
+
+function drawLaptop(){
+  if (laptopCanvas.getContext) {
+    var laptop = laptopCanvas.getContext('2d');
+    laptop.rotate(5*Math.PI/180);
+    //make the bottom
+    laptop.fillStyle = '#494941';
+    laptop.beginPath();
+    laptop.moveTo(40,250);
+    laptop.lineTo(340,250);
+    laptop.lineTo(340,390);
+    laptop.arc(330,390,10,0,Math.PI/2)
+    laptop.lineTo(50,400);
+    laptop.arc(50,390,10,Math.PI/2, Math.PI)
+    laptop.lineTo(40,250);
+    laptop.fill();
+    laptop.strokeStyle = '#88887d';
+    laptop.lineWidth = 5;
+    laptop.lineJoin = "round";
+    laptop.stroke();
+
+    //add mousepad
+    laptop.fillStyle = '#88887d';
+    laptop.beginPath();
+    laptop.moveTo(155,395);
+    laptop.lineTo(225,395);
+    laptop.lineTo(225,350);
+    laptop.lineTo(155,350);
+    laptop.fill();
+
+    //add keyboard
+    laptop.fillStyle = '#88887d';
+    laptop.beginPath();
+    laptop.moveTo(45,345);
+    laptop.lineTo(335,345);
+    laptop.lineTo(335,275);
+    laptop.lineTo(45,275);
+    laptop.fill();
+
+    //build screen
+    laptop.fillStyle = '#494941';
+    laptop.beginPath();
+    laptop.moveTo(40,250);
+    laptop.lineTo(340,250);
+    laptop.arc(335,120,20,0,3*Math.PI/2,1);
+    laptop.lineTo(340,100);
+    laptop.lineTo(40,100);
+    laptop.arc(45,120,20,3*Math.PI/2,Math.PI,1);
+    laptop.lineTo(40,250);
+    laptop.fill();
+    laptop.strokeStyle = '#88887d';
+    laptop.lineWidth = 5;
+    laptop.lineJoin = "round";
+    laptop.stroke();
+
+    //add screen
+    laptop.fillStyle = '#000000';
+    laptop.beginPath();
+    laptop.moveTo(50,235);
+    laptop.lineTo(330,235);
+    laptop.lineTo(345,115);
+    laptop.lineTo(35,115);
+    laptop.fill();
+  }
 }
 
 init();
