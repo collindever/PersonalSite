f599ab0ac1f9701b1435deac3dce31c47d5a8904
Collin Dever <collin@iovox.com>
2018-06-25 22:30:23 -0700

    added the picture frame to the desk layout

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 var cupCanvas = document.getElementById('coffee-cup');
 var laptopCanvas = document.getElementById('laptop');
 var papersCanvas = document.getElementById('papers');
+var frameCanvas = document.getElementById('picture-frame');
 
 var creamAngle = Math.PI / 180;
 var creamRadius = 35;
@@ -17,6 +18,7 @@ function init(){
   drawLaptop();
   window.requestAnimationFrame(drawCoffeeCup);
   drawPapers();
+  drawPictureFrame();
 }
 
 function drawCoffeeCup(){
@@ -195,6 +197,64 @@ function drawPapers(){
     papers.strokeStyle = '#000000';
     papers.lineWidth = 0.25;
     papers.stroke();
+  }
+}
+
+function drawPictureFrame(){
+  if (frameCanvas.getContext) {
+    var frame = frameCanvas.getContext('2d');
+
+    // make the frame
+    frame.fillStyle = '#5b422d';
+    frame.beginPath();
+    frame.moveTo(5,15);
+    frame.lineTo(85,5);
+    frame.lineTo(95,80);
+    frame.lineTo(15,90);
+    frame.lineTo(5,15);
+    frame.fill();
+
+    //make the matting
+    frame.fillStyle = '#f9fad0';
+    frame.beginPath();
+    frame.moveTo(15,24);
+    frame.lineTo(78,16);
+    frame.lineTo(85,72);
+    frame.lineTo(23,80);
+    frame.lineTo(15,24);
+    frame.fill();
+
+    //make the picture
+    frame.fillStyle = '#86ced4';
+    frame.beginPath();
+    frame.moveTo(17,26);
+    frame.lineTo(76,18);
+    frame.lineTo(83,70);
+    frame.lineTo(25,78);
+    frame.lineTo(17,26);
+    frame.fill();
+
+    // make the first mountain
+    frame.fillStyle = '#ade4a2';
+    frame.beginPath();
+    frame.moveTo(83,70);
+    frame.lineTo(83,55);
+    frame.lineTo(65,35);
+    frame.lineTo(50,75);
+    frame.fill();
+
+    // make the second mountain
+    frame.fillStyle = '#77a26e';
+    frame.beginPath();
+    frame.moveTo(27,77);
+    frame.lineTo(40,47);
+    frame.lineTo(56,60);
+    frame.lineTo(50,75);
+    frame.fill();
+
+    //make cloud
+    frame.fillStyle = '#ffffff';
+    frame.beginPath();
   }
 }
 
