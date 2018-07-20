aec9e1bd412f0d9b319ebac780a9e8344948004a
Collin Dever <collin@iovox.com>
2018-07-20 01:02:03 -0700

    general fix up found good font color removed title spaced shit out resized canvas to fit tighter

--- static/main.js
+++ static/main.js
@@ -102,13 +102,13 @@ function drawLaptop(){
     //make the bottom
     laptop.fillStyle = '#494941';
     laptop.beginPath();
-    laptop.moveTo(40,250);
-    laptop.lineTo(340,250);
-    laptop.lineTo(340,390);
-    laptop.arc(330,390,10,0,Math.PI/2);
-    laptop.lineTo(50,400);
-    laptop.arc(50,390,10,Math.PI/2, Math.PI);
-    laptop.lineTo(40,250);
+    laptop.moveTo(40,160);
+    laptop.lineTo(340,160);
+    laptop.lineTo(340,300);
+    laptop.arc(330,300,10,0,Math.PI/2);
+    laptop.lineTo(50,310);
+    laptop.arc(50,300,10,Math.PI/2, Math.PI);
+    laptop.lineTo(40,160);
     laptop.fill();
     laptop.strokeStyle = '#88887d';
     laptop.lineWidth = 5;
@@ -118,31 +118,31 @@ function drawLaptop(){
     //add mousepad
     laptop.fillStyle = '#88887d';
     laptop.beginPath();
-    laptop.moveTo(155,395);
-    laptop.lineTo(225,395);
-    laptop.lineTo(225,350);
-    laptop.lineTo(155,350);
+    laptop.moveTo(155,305);
+    laptop.lineTo(225,305);
+    laptop.lineTo(225,260);
+    laptop.lineTo(155,260);
     laptop.fill();
 
     //add keyboard
     laptop.fillStyle = '#88887d';
     laptop.beginPath();
-    laptop.moveTo(45,345);
-    laptop.lineTo(335,345);
-    laptop.lineTo(335,275);
-    laptop.lineTo(45,275);
+    laptop.moveTo(45,255);
+    laptop.lineTo(335,255);
+    laptop.lineTo(335,185);
+    laptop.lineTo(45,185);
     laptop.fill();
 
     //build screen
     laptop.fillStyle = '#494941';
     laptop.beginPath();
-    laptop.moveTo(40,250);
-    laptop.lineTo(340,250);
-    laptop.arc(335,120,20,0,3*Math.PI/2,1);
-    laptop.lineTo(340,100);
-    laptop.lineTo(40,100);
-    laptop.arc(45,120,20,3*Math.PI/2,Math.PI,1);
-    laptop.lineTo(40,250);
+    laptop.moveTo(40,160);
+    laptop.lineTo(340,160);
+    laptop.arc(335,30,20,0,3*Math.PI/2,1);
+    laptop.lineTo(340,10);
+    laptop.lineTo(40,10);
+    laptop.arc(45,30,20,3*Math.PI/2,Math.PI,1);
+    laptop.lineTo(40,160);
     laptop.fill();
     laptop.strokeStyle = '#88887d';
     laptop.lineWidth = 5;
@@ -152,10 +152,10 @@ function drawLaptop(){
     //add screen
     laptop.fillStyle = '#000000';
     laptop.beginPath();
-    laptop.moveTo(50,235);
-    laptop.lineTo(330,235);
-    laptop.lineTo(345,115);
-    laptop.lineTo(35,115);
+    laptop.moveTo(50,145);
+    laptop.lineTo(330,145);
+    laptop.lineTo(345,25);
+    laptop.lineTo(35,25);
     laptop.fill();
   }
 }
