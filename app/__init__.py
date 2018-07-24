9c74e0c65062a6a42547b4a6a21d6d85e8085863
Collin Dever <collin@iovox.com>
2018-07-24 07:56:29 -0700

    took a while but restructured to a more robust project so I can include React

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# Import flask and template operators
+from flask import Flask, render_template
+
+
+def create_app(config_filename):
+    # Define the WSGI application object
+    app = Flask(__name__)
+
+    # Configurations
+    app.config.from_object(config_filename)
+
+    @app.route('/')
+    def index():
+    	return render_template('index.html')
+
+    return app
+
---
+++ config.py
@@ -0,0 +1,16 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+from os import path
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
