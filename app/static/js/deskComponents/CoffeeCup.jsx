43f3b002443483caaea14e2b379d0dbd3aa6a440
Collin Dever <collin@iovox.com>
2018-07-27 23:53:01 -0700

    Have recreated all the components of the master branch in react will move merge into master and move forward with add new features

--- app/static/js/deskComponents/CoffeeCup.jsx
+++ app/static/js/deskComponents/CoffeeCup.jsx
@@ -1,145 +1,93 @@
 import React, {Component} from 'react';
 
 export default class CoffeeCup extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {creamX: 65, creamY: 40, creamAngle: Math.PI / 180, creamAcceleration: 1, accelerateCream: false};
+      this.updateCanvas = this.updateCanvas.bind(this);
+      this.onClick = this.onClick.bind(this);
+  }
+
   componentDidMount() {
-        this.updateCanvas();
+      requestAnimationFrame(this.updateCanvas);
+  }
+
+
+  updateCanvas() {
+      const cup = this.refs.coffeeCup.getContext('2d');
+
+      // make the bottom of the cup
+      cup.beginPath();
+      cup.arc(42.5,75,17,.8,3.2);
+      cup.closePath();
+      cup.fillStyle = '#e8e29f';
+      cup.fill();
+
+      //make the cup sides
+      cup.fillStyle = '#e8e29f';
+      cup.beginPath();
+      cup.moveTo(20,50);
+      cup.lineTo(68,65);
+      cup.lineTo(55,87);
+      cup.lineTo(25,75);
+      cup.fill();
+
+      // put the coffee in the cup
+      cup.fillStyle = '#522f2e';
+      cup.beginPath();
+      cup.arc(47,40,30,0,2*Math.PI);
+      cup.fill();
+
+      // make the cup rim
+      var radGrd = cup.createRadialGradient(47,40,30,47,40,2);
+      radGrd.addColorStop(0,"#fbffcd");
+      radGrd.addColorStop(1,"white");
+      cup.strokeStyle = radGrd;
+      cup.lineWidth = 5;
+      cup.stroke();
+
+      //add the cream
+      cup.fillStyle = '#fdffdd';
+      cup.beginPath();
+      cup.arc(this.state.creamX,this.state.creamY,3,0,2*Math.PI);
+      cup.fill();
+
+      //request animation frame
+      requestAnimationFrame(this.updateCanvas);
+      //move along the cream
+      this.animateCream();
+
+  }
+
+  animateCream(){
+    const creamRadius = 15;
+    const cx = 47;
+    const cy = 40;
+    // if the cup has been clicked we stir the coffee
+    if(this.state.accelerateCream == true){
+      if(this.state.creamAcceleration > 1){
+        this.state.creamAcceleration -= 0.1;
+      }else {
+        this.state.accelerateCream = false;
+      }
     }
-    componentDidUpdate() {
-        this.updateCanvas();
-    }
 
-    updateCanvas() {
-        const creamX = 65;
-        const creamY = 40;
-        const cup = this.refs.coffeeCup.getContext('2d');
+    // increase the angle of rotation
+    this.state.creamAngle += this.state.creamAcceleration * Math.PI / 300;
 
-        // make the bottom of the cup
-        cup.beginPath();
-        cup.arc(42.5,75,17,.8,3.2);
-        cup.closePath();
-        cup.fillStyle = '#e8e29f';
-        cup.fill();
+    // calculate the new ball.x / ball.y
+    this.state.creamX = cx + creamRadius * Math.cos(this.state.creamAngle);
+    this.state.creamY = cy + creamRadius * Math.sin(this.state.creamAngle);
+  }
 
-        //make the cup sides
-        cup.fillStyle = '#e8e29f';
-        cup.beginPath();
-        cup.moveTo(20,50);
-        cup.lineTo(68,65);
-        cup.lineTo(55,87);
-        cup.lineTo(25,75);
-        cup.fill();
+  onClick() {
+    this.state.creamAcceleration = 20;
+    this.state.accelerateCream = true;
+  }
 
-        // put the coffee in the cup
-        cup.fillStyle = '#522f2e';
-        cup.beginPath();
-        cup.arc(47,40,30,0,2*Math.PI);
-        cup.fill();
-
-        // make the cup rim
-        var radGrd = cup.createRadialGradient(47,40,30,47,40,2);
-        radGrd.addColorStop(0,"#fbffcd");
-        radGrd.addColorStop(1,"white");
-        cup.strokeStyle = radGrd;
-        cup.lineWidth = 5;
-        cup.stroke();
-
-        //add the cream
-        cup.fillStyle = '#fdffdd';
-        cup.beginPath();
-        cup.arc(creamX,creamY,3,0,2*Math.PI);
-        cup.fill();
-    }
-
-    render() {
-         return (
-             <canvas ref='coffeeCup' id='coffeeCup' height={100} width={85} />
-         );
-    }
+  render() {
+       return (
+           <canvas ref='coffeeCup' id='coffeeCup' onClick={this.onClick} height={100} width={85} />
+       );
+  }
 }
-
-
-
-// var cupCanvas = document.getElementById('coffee-cup');
-//
-// var creamAngle = Math.PI / 180;
-// var creamRadius = 15;
-// var creamX = 65;
-// var creamY = 40;
-// var creamAcceleration = 1;
-// var accelerateCream = false;
-// var t = 0;
-// var cx = 47;
-// var cy = 40;
-//
-//
-// function init(){
-//   window.requestAnimationFrame(drawCoffeeCup);
-// }
-//
-// function drawCoffeeCup(){
-//   if (cupCanvas.getContext) {
-//     var cup = cupCanvas.getContext('2d');
-//     cupCanvas.onclick = function(e) {
-//       //stir the cream max speed coefficient
-//       creamAcceleration = 20;
-//       accelerateCream = true;
-//     }
-//
-//     // make the bottom of the cup
-//     cup.beginPath();
-//     cup.arc(42.5,75,17,.8,3.2);
-//     cup.closePath();
-//     cup.fillStyle = '#e8e29f';
-//     cup.fill();
-//
-//     //make the cup sides
-//     cup.fillStyle = '#e8e29f';
-//     cup.beginPath();
-//     cup.moveTo(20,50);
-//     cup.lineTo(68,65);
-//     cup.lineTo(55,87);
-//     cup.lineTo(25,75);
-//     cup.fill();
-//
-//     // put the coffee in the cup
-//     cup.fillStyle = '#522f2e';
-//     cup.beginPath();
-//     cup.arc(47,40,30,0,2*Math.PI);
-//     cup.fill();
-//
-//     // make the cup rim
-//     var radGrd = cup.createRadialGradient(47,40,30,47,40,2);
-//     radGrd.addColorStop(0,"#fbffcd");
-//     radGrd.addColorStop(1,"white");
-//     cup.strokeStyle = radGrd;
-//     cup.lineWidth = 5;
-//     cup.stroke();
-//
-//     //add the cream
-//     cup.fillStyle = '#fdffdd';
-//     cup.beginPath();
-//     cup.arc(creamX,creamY,3,0,2*Math.PI);
-//     cup.fill();
-//
-//     window.requestAnimationFrame(drawCoffeeCup);
-//     animateCoffeeCup()
-//   }
-// }
-//
-// function animateCoffeeCup(){
-//   // if the cup has been clicked we stir the coffee
-//   if(accelerateCream == true){
-//     if(creamAcceleration > 1){
-//       creamAcceleration -= 0.1;
-//     }else {
-//       accelerateCream = false;
-//     }
-//   }
-//
-//   // increase the angle of rotation
-//   creamAngle += creamAcceleration * Math.PI / 300;
-//
-//   // calculate the new ball.x / ball.y
-//   creamX = cx + creamRadius * Math.cos(creamAngle);
-//   creamY = cy + creamRadius * Math.sin(creamAngle);
-// }
